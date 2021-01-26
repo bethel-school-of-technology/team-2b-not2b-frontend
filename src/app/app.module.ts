e55a6b729632978ca98db985d07c7c1cea61bd60
Henry Motu <henrymotunui@gmail.com>
2021-01-25 14:55:32 -1000

    Added Nav module for dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SignupPageComponent } from './components/signup-page/signup-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
@@ -12,10 +20,18 @@ import { SignupPageComponent } from './components/signup-page/signup-page.compon
     AppComponent,
     LandingPageComponent,
     LoginPageComponent,
-    SignupPageComponent
+    SignupPageComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
